99e396ceae438496b58d771e591815cb891fc844
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2020-09-15 14:48:03 -0400

    Bon travail !
    Toutes les notions que je voulais voir sont présentes donc la compétence est atteinte !!!
    Programme bien commenté.
    Mais attention, surtout en examen, d'utiliser seulement les notions vues jusqu'à présent pour que l'évaluation soit juste et équitable.

--- Exercice 1/PairImpair.cpp
+++ Exercice 1/PairImpair.cpp
@@ -1,42 +1,44 @@
 //Nom : Mikael Lagasse
 //Date : 2020/09/08
-//But :  un programme qui lit un entier et qui détermine et affiche s’il s’agit d’un entier pair d’un entier impair.
+//But :  un programme qui lit un entier et qui dÃ©termine et affiche sâ€™il sâ€™agit dâ€™un entier pair dâ€™un entier impair.
 
 #include <iostream>				//cin et cout , on en aura toujours de besoin de iostream.
 
-using namespace std;			//Parce que écrire std:: c'est tannant.
+using namespace std;			//Parce que Ã©crire std:: c'est tannant.
 
 
 void main()						//On commence par main.
 {
-	double input;				//on déclare la variable ou notre input sera conservé (voir #convert pour une explication sur pourquoi j'ai utiliser double et non int)
+	double input;				//on dÃ©clare la variable ou notre input sera conservÃ© (voir #convert pour une explication sur pourquoi j'ai utiliser double et non int)
 	int input2;					//J'expliquerais l'utilisation de cette variable un peu plus tard.
-	int reponse;				//Ici , c'est avec cette variable qu'on va etre capable de vérifier si le nombre est pair ou non.
+	int reponse;				//Ici , c'est avec cette variable qu'on va etre capable de vÃ©rifier si le nombre est pair ou non.
 	//int reponse2;
-	setlocale(LC_ALL, "");		//accents et tout.	//Bon , ici , on regarde si input est un nombre pair ou pas. Si "reponse" est égal à 1 , c'est impair , si c'est égal à 2 , c'est pair.
-	while (true)				//Un while loop qui recommence tant qu'on lui donne pas une reponse valide. voir le commentaire #validation pour comprendre les critères.
+	setlocale(LC_ALL, "");		//accents et tout.	//Bon , ici , on regarde si input est un nombre pair ou pas. Si "reponse" est Ã©gal Ã  1 , c'est impair , si c'est Ã©gal Ã  2 , c'est pair.
+	while (true)				//Un while loop qui recommence tant qu'on lui donne pas une reponse valide. voir le commentaire #validation pour comprendre les critÃ¨res.
 	{
 		cout << "Veuillez me donner un chiffre.\n";
-		cin >> input;			//Les chiffres entrer par l'utilisateur seront storer dans la variable input.
+		cin >> input;			//FR : Les chiffres entrer par l'utilisateur seront storer dans la variable input.
 		cout << "\n";			//skip une ligne
 		if (input > INT_MAX | input < INT_MIN)		//#validation : Verification a savoir si l'utilisateur depasse INT_MIN/MAX.
 		{
 			cout << "Un chiffre qui ne depasse pas : " << INT_MAX << " ou " << INT_MIN << " .\n";	//Ce message s'affiche si l'utilisateur nous donner un chiffre qui depasse INT_MIN ou INT_MAX , car depasser ses valeurs brise le programme.
 			cout << "\n";		//skip une autre ligne.
+			// KM : Pas une bonne technique de programme le exit
 			exit;				//On relance le loop.
 		}
+		// KM : Break n'est pas non plus une bonne technique pour sortir d'une boucle
 		else
 		{
 			break;			//Si on donne une reponse qui est valide , on continue.
 		}
 	}
-	input2 = int(input);	//#convert : On converti la reponse de l'utilisateur en Integer , car sinon on ne pourra pas utiliser %.
-	reponse = input2 % 2;	//Modulo , parce que c'est sa qui est sa.
+	input2 = int(input);	//FR : #convert : On converti la reponse de l'utilisateur en Integer , car sinon on ne pourra pas utiliser %.
+	reponse = input2 % 2;	//FR : Modulo , parce que c'est sa qui est sa.
 	if (reponse == 0)		//Si la reponse est 0 , c'est parce que c'est pair , donc on le dit a l
 	{
 		cout << input2 << " est un nombre pair!\n";
 	}
-	else					//Si c'est pas 0 , c'est parce que c'est impair. On l'averti à propos de sa aussi.
+	else					//FR : Si c'est pas 0 , c'est parce que c'est impair. On l'averti Ã  propos de sa aussi.
 	{
 		cout << input2 << " est un nombre impair!\n";
 	}
